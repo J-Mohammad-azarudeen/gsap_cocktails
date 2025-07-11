b2e41a9d166a44c76b45d686a8ab16a1563be99a
DarkAzarWorld <azarrock1000@gmail.com>
2025-07-11 19:28:39 +0530

    Implemented the hero Section scroll animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/all'
 import { useGSAP } from '@gsap/react'
 import { SplitText } from 'gsap/all'
+import { useMediaQuery } from 'react-responsive'
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
+
+  const videoRef = useRef();
+
+  const isMobile = useMediaQuery({maxWidth: 767});
 
   useGSAP(() => {
     const heroSplit = new SplitText('.title', {type:'chars,words'});
     const paragraphSplit = new SplitText('.subtitle', {type:'lines'});
 
-    heroSplit.chars.forEach((char) =>  char.classList.add('text-gradient'));
+    heroSplit.chars.forEach((char) => char.classList.add('text-gradient'));
 
     gsap.from(heroSplit.chars, {
       yPercent:100,
@@ -39,6 +46,27 @@ const Hero = () => {
     })
     .to('.right-leaf',{x:200},0)
     .to('.left-leaf',{x:-200},0)
+
+    const startValue = isMobile ? 'top 50%' : 'center 60%';
+    const endValue = isMobile ? '120% top' : 'bottom top';
+
+    const tl = gsap.timeline({
+      scrollTrigger:{
+        trigger:'video',
+        start:startValue,
+        endValue:endValue,
+        scrub:true,
+        pin:true
+      }
+    })
+
+    videoRef.current.onloadedmetadata= () => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+        
+      })
+    }
+
   },[]);
 
   return (
@@ -73,6 +101,10 @@ const Hero = () => {
     </div>
      </div>
     </section>
+
+    <div className='video absolute inset-0'>
+    <video ref={videoRef} src="/videos/output.mp4" muted playsInline preload='auto'  />
+    </div>
     </>
   )
 }
